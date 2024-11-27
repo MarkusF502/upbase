07c71990a872263387d43d681967c1884c36605e
Marcos Filipe P. Marinho <markusfilipe502@gmail.com>
2024-11-27 20:54:06 -0300

    solução tela 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header className='status-bar'></header>
+      <section>
+      <i><FaArrowLeft/></i>
+      <p>Para acessar sua conta no Poundsflats, por favor, insira seu número de telefone abaixo</p>
+      </section>
+      <main>
+        <input type='number' placeholder='Número de Celular'/>
+        <p className='sms'>Para confirmar a sua identidade, enviaremos um código via SMS para o número informado</p>
+        <button className='cont'>Continuar</button>
+
+        <section className='login'>
+          
+          <div className='ou'><span>OU</span></div>
+          <button><FaFacebook className='icon'/> Continuar com o Facebook</button>
+          <button><FaGoogle className='icon'/> Continuar com o Google</button>
+          <button><MdOutlineMailOutline className='icon'/> Continuar com o Email</button>
+        </section>
+      </main>
     </div>
   );
 }
+
 
 export default App;
